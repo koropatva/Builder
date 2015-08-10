8b14b82cf3672d00003816ffbf17e99e5d16910c
Andrew Koropatva <akoropatva@chegg.com>
2015-08-10 16:53:41 +0300

    Moved database configurations into the variables, that we can fill from the build.gradle

--- src/main/groovy/com/hmel/builder/BuilderPlugin.groovy
+++ src/main/groovy/com/hmel/builder/BuilderPlugin.groovy
@@ -1,24 +1,19 @@
 package com.hmel.builder;
 
-import org.gradle.api.*;
-import com.hmel.builder.schema.SchemaBuilder;
-import com.hmel.builder.schema.properties.Schema;
-import com.hmel.builder.schema.properties.Datasource;
+import org.gradle.api.*
+
+import com.hmel.builder.schema.SchemaBuilder
+import com.hmel.builder.schema.properties.Datasource
+import com.hmel.builder.schema.properties.Schema
 
 class BuilderPlugin implements Plugin<Project> {
+
 	def void apply(Project project) {
-		Schema schema = new Schema()
-		schema.sqlComment = "#"
-		schema.uriSqlFiles = "sql-files/"
-		Datasource datasource = new Datasource()
-		datasource.user = "root"
-		datasource.password = ""
-		datasource.url = "jdbc:mysql://localhost:3306/"
-		datasource.driver = "com.mysql.jdbc.Driver"
-		schema.datasource = datasource
+		project.extensions.create('schema', Schema)
+		project.extensions.create('datasource', Datasource)
+
 		project.task('update') << {
-			new SchemaBuilder().update(schema)
+			new SchemaBuilder().update(project)
 		}
-		
 	}
 }
--- src/main/groovy/com/hmel/builder/schema/SchemaBuilder.groovy
+++ src/main/groovy/com/hmel/builder/schema/SchemaBuilder.groovy
@@ -1,16 +1,21 @@
 package com.hmel.builder.schema;
 
+import java.sql.SQLException;
+
+import org.gradle.api.*;
+
 import groovy.sql.Sql;
 import groovy.io.FileType;
+
 import com.hmel.builder.schema.properties.Schema;
 
 class SchemaBuilder {
-	
-	private def getSqlFile(def email, String sqlFiles) {
+
+	private def getSqlFile(def email, String sqlFiles, String sqlSuffix) {
 		def lst = []
 		def dir = new File(sqlFiles)
 		dir.eachFileRecurse(FileType.FILES) {file ->
-			if(file.name.endsWith(".sql")) {
+			if(file.name.endsWith(sqlSuffix)) {
 				def flag = false
 				file.eachLine() { line ->
 					if(line.toString().contains(email))
@@ -18,31 +23,39 @@ class SchemaBuilder {
 				}
 				if(!flag)
 					lst << file
-				else 
+				else
 					println "${file.name} was runned before and skipped!"
 			}
 		}
-	return lst
+		return lst
 	}
 
-	public def update(Schema schema) {
-		def sql = Sql.newInstance(schema.datasource.url, schema.datasource.user, schema.datasource.password, schema.datasource.driver)
-		def email = ""
-		"git config --local user.email".execute().text.eachLine() { email+= it}
-		def sqlComment = schema.sqlComment
-		getSqlFile(email, schema.uriSqlFiles).sort{it.name}.each { sqlSyntax ->
-			println "Running ${sqlSyntax} script..."
-			String query = ""
-			sqlSyntax.eachLine() { line ->
-				if(line?.trim() && !line.startsWith(sqlComment))
-					query += line.trim()
-				if(query?.trim() && query.endsWith(";")) {
-					println "\t${query}...\n"
-					sql.execute(query)
-					query = ""
+	public def update(Project project) {
+		println "INFO: URL - ${project.datasource.url}"
+		println "INFO: USER - ${project.datasource.user}"
+		println "INFO: PASSWORD - ${project.datasource.password}"
+		println "INFO: DRIVER - ${project.datasource.driver}"
+		try{
+			def sql = Sql.newInstance(project.datasource.url, project.datasource.user, project.datasource.password, project.datasource.driver)
+			def email = ""
+			"git config --local user.email".execute().text.eachLine() { email+= it}
+			def sqlComment = project.schema.sqlComment
+			getSqlFile(email, project.schema.uriSqlFiles, project.schema.sqlSuffix).sort{it.name}.each { sqlSyntax ->
+				println "Running ${sqlSyntax} script..."
+				String query = ""
+				sqlSyntax.eachLine() { line ->
+					if(line?.trim() && !line.startsWith(sqlComment))
+						query += line.trim()
+					if(query?.trim() && query.endsWith(";")) {
+						println "\t${query}...\n"
+						sql.execute(query)
+						query = ""
+					}
 				}
+				sqlSyntax.append "\n${sqlComment}${email}"
 			}
-			sqlSyntax.append "\n${sqlComment}${email}"
+		}catch(SQLException e){
+			println "ERROR: ${e.getMessage()}"
 		}
 	}
 }
